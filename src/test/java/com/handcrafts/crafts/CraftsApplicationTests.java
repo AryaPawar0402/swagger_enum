e2a78d92c2e2439d76157181bc3adc03b0f49d9f
Arya Pawar <aryapawar0402@gmail.com>
2025-06-17 13:07:59 +0530

    this is backend using enum

---
+++ src/test/java/com/handcrafts/crafts/CraftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handcrafts.crafts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
